b6ea8741317150844299d1659ba730d863c4e619
xiaojiezhou <912639584@qq.com>
2017-02-10 00:08:33 +0000

    webpack配置文件加注释

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,8 +1,12 @@
 var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
+/**加载vue-loader的配置文件**/
 var vueLoaderConfig = require('./vue-loader.conf')
 
+/**查找目录的函数,查找到的目录是 和build同级目录下的文件
+ *实际上就是根目录下的 /dir
+ * **/
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -12,10 +16,12 @@ module.exports = {
     app: './src/main.js'
   },
   output: {
-    path: config.build.assetsRoot,
+    path: config.build.assetsRoot, //config index.js输出 '/'
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
+        /**生产环境下，config index.js 输出 '/'**/
       ? config.build.assetsPublicPath
+        /**开发模式下，config index.js 输出 '/' **/
       : config.dev.assetsPublicPath
   },
   resolve: {
@@ -43,16 +49,19 @@ module.exports = {
         }
       },
       {
+        /**打包.vue文件**/
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
       },
       {
+        /**es5转换成es6执行**/
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test')]
       },
       {
+        /**将小于1000kb的图片转换成base64**/
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         query: {
