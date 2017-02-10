7be0e8eef202eea8010a7d89a872cc4564ce0d0a
xiaojiezhou <912639584@qq.com>
2017-02-10 05:49:23 +0000

    修改项目目录

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header>header</header>
     <router-view></router-view>
   </div>
 </template>
---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        data(){
+            return {}
+        }
+    }
+</script>
+
+<style type="text/css" rel="stylesheets/stylus">
+
+</style>
