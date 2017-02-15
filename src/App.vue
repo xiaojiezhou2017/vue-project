7964ba63760a72bb17a8984ea067376dccbbd221
xiaojiezhou <912639584@qq.com>
2017-02-15 02:15:07 +0000

    维修电脑后的提交

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <div id="app">
-    <header>header</header>
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <v-header></v-header>
+        <div class="tab">
+            <div class="tab-item">商品</div>
+            <div class="tab-item">评论</div>
+            <div class="tab-item">商家</div>
+        </div>
+        <div class="content">
+            i am content
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    import header from 'components/header/header.vue';
+    export default {
+        name: 'app',
+        components: {
+            'v-header': header
+        }
+    };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        我是header
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {};
+        }
+    };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    .header
+        color: red
+</style>
